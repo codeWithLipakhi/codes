7e69969ccb92190ced9119ef09cdb7419d1264b8
codeWithLipakhi <125471021+codeWithLipakhi@users.noreply.github.com>
2023-02-22 23:47:03 +0530

    Add files via upload

---
+++ c.java
@@ -0,0 +1,64 @@
+package practice_project;
+
+public class c {
+
+	 //  static m0() {} we can not declare a method only static but we can declare a method static with a return type
+	
+	
+	static void M1() {
+		M2();
+		int a =24;
+		System.out.println("a="+a);
+		
+	}
+	
+	// public  M0() {} we can not declare a method only public but we can declare a method public with a return type
+	
+	public void m1() {
+		M2();
+		long f = 1324354999000967586l;
+		System.out.println("f="+f);
+		
+	}
+	 // but we can declare a method only void that is a return type
+	void m2() {
+		M2();
+		float g=3.9f;
+		//float h=3.2; not possible to assign a bigger value to smaller data type 
+		System.out.println("g="+g);
+	}
+	// public static m0() {} we can not declare a method public static only and we can declare it with return type 
+	// changing the keyword does not make any difference
+	static public void m3() {
+		M2();
+		String s="clear the output";
+		System.out.println("s="+s);
+	}
+	
+	static void M2() {
+		m3() ;
+		byte b =127;
+		System.out.println("b="+b);
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+
+	int q=23;
+	System.out.println("q="+q);
+	
+	c c1 =new c();
+	M1();
+	c1.m1();
+	c1.m2();
+	m3();
+	
+	
+	
+	}
+
+}
